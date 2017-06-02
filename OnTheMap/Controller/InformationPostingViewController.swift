b5292d953a56cfa7e637dc3552a11bfd049a101b
Shobhit Gupta <shobhit.gupta.compscience@gmail.com>
2017-06-02 09:14:07 +0530

    update: Rearrange InformationPostingViewController and fine tune textfield keyboard experience

--- OnTheMap/Controller/InformationPostingViewController.swift
+++ OnTheMap/Controller/InformationPostingViewController.swift
@@ -12,18 +12,12 @@ import MapKit
 
 class InformationPostingViewController: UIViewController {
     
-    enum InformationPostingState {
-        case addLocation
-        case busy
-        case addLink(forMapString: String, latitude: Double, longitude: Double)
-    }
-    
-
+    // MARK: IBOutlets
     @IBOutlet weak var addLinkStackView: UIStackView!
     @IBOutlet weak var addLocationStackView: UIStackView!
     @IBOutlet weak var overlaySubmitStackView: UIStackView!
     @IBOutlet weak var cancelButton: UIButton!
-
+    
     @IBOutlet weak var addressTextField: UITextField!
     @IBOutlet weak var linkTextField: UITextField!
     @IBOutlet weak var mapView: MKMapView!
@@ -31,47 +25,33 @@ class InformationPostingViewController: UIViewController {
     @IBOutlet weak var busyView: BusyView!
     
     
-    var currentState: InformationPostingState = .addLocation {
+    // MARK: Private variables and types
+    fileprivate enum InformationPostingState {
+        case addLocation
+        case busy(title: String?, subtitle: String?)
+        case addLink(forMapString: String, latitude: Double, longitude: Double)
+    }
+    
+    
+    fileprivate var currentState: InformationPostingState = .addLocation {
         didSet {
             updateUI(to: currentState)
         }
     }
     
     
+    // MARK: UIViewController Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-    }
-    
-    
-    func updateUI(to state: InformationPostingState) {
-        
-        switch state {
-        case .addLocation:
-            busyView.dismiss()
-            UIView.fade(out: addLinkStackView, andHide: true, thenFadeIn: addLocationStackView)
-            UIView.transition(with: self.cancelButton, duration: 2.0, options: .transitionCrossDissolve, animations: { self.cancelButton.setTitleColor(UIColor(netHex: 0x325075), for: .normal) }, completion: nil)
-            overlaySubmitStackView.fadeOut { _ in
-                self.overlaySubmitStackView.isHidden = true
-            }
-            
-        case .addLink:
-            busyView.dismiss()
-            UIView.fade(out: addLocationStackView, andHide: true, thenFadeIn: addLinkStackView) { _ in
-            }
-            self.overlaySubmitStackView.fadeIn(duration: 2.0)
-            UIView.transition(with: self.cancelButton, duration: 2.0, options: .transitionCrossDissolve, animations: { self.cancelButton.setTitleColor(UIColor.white, for: .normal) }, completion: nil)
-            
-        case .busy:
-            busyView.present()
-            
-        }
-        
+        setupUI()
     }
     
 
+    // MARK: IBActions
     @IBAction func findOnMap(_ sender: Any) {
         if let address = addressTextField.text {
-            currentState = .busy
+            currentState = .busy(title: Default.BusyView.LookingUpAddress.Title,
+                                 subtitle: Default.BusyView.LookingUpAddress.Subtitle)
             CLGeocoder().geocodeAddressString(address, completionHandler: forwardGeocoded(placemarks:error:))
         }
     }
@@ -84,15 +64,14 @@ class InformationPostingViewController: UIViewController {
     
     @IBAction func submit(_ sender: Any) {
         guard case InformationPostingState.addLink(let mapString, let latitude, let longitude) = currentState else {
+            print(Error_.General.UnexpectedCurrentState(state: "\(currentState)").localizedDescriptionVerbose)
             return
         }
         
-        guard linkTextField.hasText else {
-            return
-        }
-        
-        guard let url = URL(string: linkTextField.text!) else {
-            return
+        guard let text = linkTextField.text,
+            let url = URL(string: text) else {
+                present(Error_.App.BadURL(urlString: linkTextField.text).alertController(), animated: true, completion: nil)
+                return
         }
         
         OTMModel.shared.student?.mapString = mapString
@@ -100,7 +79,8 @@ class InformationPostingViewController: UIViewController {
         OTMModel.shared.student?.longitude = longitude
         OTMModel.shared.student?.mediaURL = url
         
-        currentState = .busy
+        currentState = .busy(title: Default.BusyView.PostStudentLocation.Title,
+                             subtitle: Default.BusyView.PostStudentLocation.Subtitle)
         
         OTMModel.shared.postStudentLocation { (success, error) in
             guard success else {
@@ -109,24 +89,80 @@ class InformationPostingViewController: UIViewController {
                 }
                 return
             }
+            Default.Notification_.StudentsLocationRefreshRequested.post()
             self.dismiss(animated: true, completion: nil)
-            
         }
+        
     }
 
 }
 
 
+//******************************************************************************
+//                              MARK: User Interface
+//******************************************************************************
+extension InformationPostingViewController {
+    
+    fileprivate func setupUI() {
+        setupAddressTextField()
+    }
+    
+    
+    private func setupAddressTextField() {
+        addressTextField.becomeFirstResponder()
+    }
+    
+    
+    fileprivate func updateUI(to state: InformationPostingState) {
+        
+        switch state {
+        case .addLocation:
+            busyView.dismiss()
+            UIView.fade(out: addLinkStackView, andHide: true, thenFadeIn: addLocationStackView)
+            UIView.transition(with: self.cancelButton, duration: 2.0, options: .transitionCrossDissolve, animations: { self.cancelButton.setTitleColor(/*UIColor(netHex: 0x325075)*/ ArtKit.secondaryColor, for: .normal) }, completion: nil)
+            overlaySubmitStackView.fadeOut { _ in
+                self.overlaySubmitStackView.isHidden = true
+            }
+            addressTextField.becomeFirstResponder()
+            
+        case .addLink:
+            busyView.dismiss()
+            UIView.fade(out: addLocationStackView, andHide: true, thenFadeIn: addLinkStackView) { _ in
+            }
+            self.overlaySubmitStackView.fadeIn(duration: 1.0)
+            UIView.transition(with: self.cancelButton,
+                              duration: 1.0,
+                              options: .transitionCrossDissolve,
+                              animations: { self.cancelButton.setTitleColor(UIColor.white, for: .normal) }) { _ in
+                self.linkTextField.becomeFirstResponder()
+            }
+            
+        case let .busy(title, subtitle):
+            addressTextField.resignFirstResponder()
+            linkTextField.resignFirstResponder()
+            busyView.title = title
+            busyView.subtitle = subtitle
+            busyView.present()
+            
+        }
+        
+    }
+    
+    
+}
+
+
+//******************************************************************************
+//                              MARK: Geocoding
+//******************************************************************************
 extension InformationPostingViewController {
     
     func forwardGeocoded(placemarks: [CLPlacemark]?, error: Error?) -> Void {
-        // TODO: Present an alert view for user in case of an error
         guard error == nil else {
             currentState = .addLocation
             if let error = error {
                 present(error.alertController(), animated: true, completion: nil)
             }
-            //print(error!.localizedDescription)
             return
         }
         
@@ -134,7 +170,6 @@ extension InformationPostingViewController {
             let coordinates = placemark.location?.coordinate else {
                 currentState = .addLocation
                 present(Error_.App.AddressNotFound.alertController(), animated: true, completion: nil)
-                //print("Couldn't find the address!")
                 return
         }
         
@@ -147,5 +182,27 @@ extension InformationPostingViewController {
     }
     
 }
+
+
+//******************************************************************************
+//                              MARK: UITextField
+//******************************************************************************
+extension InformationPostingViewController: UITextFieldDelegate {
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == addressTextField {
+            findOnMap(textField)
+        } else if textField === linkTextField{
+            submit(textField)
+        }
+        return true
+    }
+    
+}
+
+
+
+
+
 
 
